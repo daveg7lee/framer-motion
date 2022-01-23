438eb0c9eba5123e4a61ee1fd049c55610e826c8
Dave <daveg7lee@gmail.com>
2022-01-23 19:50:25 +0900

    Basic Animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import './App.css';
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -9,18 +9,22 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Box = styled.div`
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
-  border-radius: 10px;
+  border-radius: 25px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box />
+      <Box
+        initial={{ scale: 0 }}
+        animate={{ scale: 1, rotate: 180 }}
+        transition={{ type: 'spring', stiffness: 260, damping: 20, delay: 0.5 }}
+      />
     </Wrapper>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from 'styled-components';
 import App from './App';
 import { darkTheme } from './styles/theme';
 import { createGlobalStyle } from 'styled-components';
+
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
 html, body, div, span, applet, object, iframe,
