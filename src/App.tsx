b643ca7521aad5705f7160db29a343ae5c420c84
Dave <daveg7lee@gmail.com>
2022-01-24 19:22:28 +0900

    Gestures part Two

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { useRef } from 'react';
 import { motion } from 'framer-motion';
 
 const Wrapper = styled.div`
@@ -10,34 +11,36 @@ const Wrapper = styled.div`
 `;
 
 const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
+  width: 80px;
+  height: 80px;
   background-color: white;
-  border-radius: 25px;
+  border-radius: 20%;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const BoxVar = {
-  hover: {
-    scale: 1.5,
-    rotate: 90,
-  },
-  tap: {
-    borderRadius: '100%',
-    scale: 1,
-    rotate: -90,
-  },
-};
+const Container = styled(motion.div)`
+  width: 160px;
+  height: 160px;
+  background-color: rgba(225, 225, 225, 0.4);
+  border-radius: 20%;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 function App() {
+  const ContainerRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <Box
-        drag
-        variants={BoxVar}
-        whileHover={BoxVar.hover}
-        whileTap={BoxVar.tap}
-      />
+      <Container dragDirectionLock ref={ContainerRef}>
+        <Box
+          drag
+          dragConstraints={ContainerRef}
+          dragSnapToOrigin
+          dragElastic={0.7}
+        />
+      </Container>
     </Wrapper>
   );
 }
